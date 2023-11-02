d26ead8ca8af3fc586daf6515a228b8c0909e2db
Vikrant <06learntocode11@gmial.com>
2023-11-02 21:39:10 +0530

    logic part done

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ let btn = document.querySelector("#btn");
 let txtInput = document.querySelector("#txt-input");
 let outputDiv = document.querySelector("#output");
 
+// let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+let serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+
+function translateYoda(text) {
+        return  serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(err) {
+        console.log(`error: ${err}`);
+        alert("something went wrong, please try again");
+}
+
 function clickHandle() {    
-        outputDiv.innerText = `output: ${txtInput.value}`;
+        let inputText = txtInput.value; //taking input
+
+        // calling server for processing
+        fetch(translateYoda(inputText))
+        .then(response => response.json())
+        .then(json => {
+                let translatedTxt = json.contents.translated
+                outputDiv.innerText = translatedTxt  // output
+        })
+        .catch(errorHandler)
 }
 
 btn.addEventListener("click", clickHandle);
